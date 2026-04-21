b9ae8d511185b1679abbbc5f97c7e7ad4fe9120a
Henry Blancas <henryblancas23@gmail.com>
2026-04-21 16:06:09 -0500

    Agregar configuración de CodeDeploy

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hola, tu app funciona en SANRAMON');
+  res.send(`
+    <html>
+      <head><title>Mi App CodeDeploy</title></head>
+      <body style="font-family: Arial; text-align: center; padding: 50px;">
+        <h1> Aplicación desplegada con AWS CodeDeploy SAN RAMON TUS NOMBRES Y APELLIDOS COMPLETOS</h1>
+        <p>Versión: 1.0.0</p>
+        <p>Desplegada el: ${new Date().toISOString()}</p>
+      </body>
+    </html>
+  `);
 });
 
 app.listen(PORT, () => {
-  console.log(`Servidor en http://localhost:${PORT}`);
+  console.log(`Servidor escuchando en http://localhost:${PORT}`);
 });
